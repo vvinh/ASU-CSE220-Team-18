8426aa57276f8766eb7416a5b9f23b28a95a46e0
tdzamora <tdzamora@general2.asu.edu>
2014-02-21 16:10:51 -0700

    wat

--- print.c
+++ print.c
@@ -9,7 +9,6 @@ void print_line(char line[], char source_name_to_print[], char date_to_print[])
     static int line_count = MAX_LINES_PER_PAGE;
 
 	// This line is for testing only !!!!!!!!!!!!!!!
-<<<<<<< HEAD
 	printf("%s\n", source_name_to_print);
 	
 	if (++line_count > MAX_LINES_PER_PAGE) {
@@ -37,33 +36,4 @@ static void print_page_header(char source_name[], char date[])
 	//Missing code here
 	page_number++;
 	printf("Page\t%d\t%s\t%s\n", page_number, source_name, date);
-
-	
-=======
-	printf(source_name_to_print);
-
-    if (++line_count > MAX_LINES_PER_PAGE)
-    {
-        /* Missing Code Here */
-    }
-    if (strlen(line) > MAX_PRINT_LINE_LENGTH)
-    {
-        /* Missing Code Here */
-    }
-    if (save_chp)
-    {
-        /* Missing Code Here */
-    }
-    /* Missing Code Here */
-    if (save_chp)
-    {
-        /* Missing Code Here */
-    }
-}
-static void print_page_header(char source_name[], char date[])
-{
-    static int page_number = 0;
->>>>>>> 681fc86cc20bbc2cae3a4d31c3305d2ddc8195ac
-
-    /* Missing Code Here */
 }
