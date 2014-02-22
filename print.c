f329b511632cd64a56f9b40dcdc12b1ee860004a
vvinh <vvinh@asu.edu>
2014-02-21 19:13:51 -0700

    latest

--- print.c
+++ print.c
@@ -1,5 +1,5 @@
 #include "common.h"
-//2014.02.21.19
+
 static void print_page_header(char source_name[], char date[]);
 
 void print_line(char line[], char source_name_to_print[], char date_to_print[])
@@ -8,21 +8,22 @@ void print_line(char line[], char source_name_to_print[], char date_to_print[])
     char *save_chp = NULL;
     static int line_count = MAX_LINES_PER_PAGE;
 
+	// This line is for testing only !!!!!!!!!!!!!!!
+	//printf("TEST%s", source_name_to_print);
 	
 	if (++line_count > MAX_LINES_PER_PAGE) {
 		/* Missing Code Here */
-		print_page_header(line, date_to_print);
-		line_count =0;
+		print_page_header(source_name_to_print, date_to_print);
+		line_count = 1;
 	}
-	if (strlen(source_name_to_print) > MAX_PRINT_LINE_LENGTH) {
-		save_ch = source_name_to_print[MAX_PRINT_LINE_LENGTH];
-		save_chp = source_name_to_print + MAX_PRINT_LINE_LENGTH;
-		source_name_to_print[MAX_PRINT_LINE_LENGTH] = '\0';
+	if (strlen(line) > MAX_PRINT_LINE_LENGTH) {
+		save_ch = line[MAX_PRINT_LINE_LENGTH];
+		save_chp = line + MAX_PRINT_LINE_LENGTH;
+		line[MAX_PRINT_LINE_LENGTH] = '\0';		
 		/* Missing Code Here */
 	}
-
-	printf("%s", source_name_to_print);
-
+	printf("%d%s%s", line_count, ": ", line);
+	//printf("MINE%s", source_name_to_print);
 	/*Missing Code Here*/
 	if (save_chp) {
 		/* Missing Code Here */
@@ -35,10 +36,4 @@ static void print_page_header(char source_name[], char date[])
 	//Missing code here
 	page_number++;
 	printf("Page\t%d\t%s\t%s\n", page_number, source_name, date);
-
-
-
-
-
-
 }
